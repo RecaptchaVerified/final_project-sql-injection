20b8201c9d23d93e1aad7adac5af7eca7c55b98f
RecaptchaVerified <92442458+RecaptchaVerified@users.noreply.github.com>
2024-01-24 08:18:21 +0000

    Update module.php
    its now structured properly

--- module.php
+++ module.php
@@ -11,19 +11,31 @@
 
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+  //Connection to DB
+$conn = mysqli_connect("localhost", "root", "", "final_project2", "3308");
 
-
+/*  
+//To use whilst implementing Escaping solution  
+$username = mysqli_real_escape_string($conn, $_POST["username"]);
+$password = mysqli_real_escape_string($conn, $_POST["password"]);
+*/
+  
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-//db connection
-$conn = mysqli_connect("localhost", "root", "", "final_project2", "3306");
+
+// Injectable code
+$sql = "SELECT Username, Password FROM customers where Username = '$username' AND Password = '$password'";
+
+$result = mysqli_query($conn, $sql);
+$count = mysqli_num_rows($result);
 
 
 
-
-//Fixed solution 1 prepared statements
-/*
+/*  
+//Fixed solution 2 Prepared Statements
 $stmt = mysqli_stmt_init($conn);
 $sql = "SELECT Username, Password FROM customers WHERE Username = ? AND Password = ?";
 if (mysqli_stmt_prepare($stmt, $sql)) 
@@ -33,56 +45,38 @@ if (mysqli_stmt_prepare($stmt, $sql))
     $count = mysqli_num_rows($result);
 */
 
+  
+  
 /*
-// Injectable code
-$sql = "SELECT Username, Password FROM customers where Username = '$username' AND Password = '$password'";
-
-
-$result = mysqli_query($conn, $sql);
-$count = mysqli_num_rows($result);
-*/
-
-
-//$sql = "SELECT email, password FROM login where email = ? AND password = ?";
-//$stmt = mysqli_stmt_init($conn);
-//mysqli_stmt_prepare($stmt, $sql);
-//mysqli_stmt_bind_param($stmt, "ss", $email, $password);
-//mysqli_stmt_execute($stmt);
-//$result = mysqli_stmt_get_result($stmt);
-//$count = mysqli_num_rows($result);
-
-
 //Fixed solution 2 stored procedures
-//$sql = CALL 'get_login'('$email', '$password');";
-//$stmt = mysqli_stmt_init($conn);
-//mysqli_stmt_prepare($stmt, $sql);
-//mysqli_stmt_execute($stmt);
-//$result = mysqli_stmt_get_result($stmt);
-//$count = mysqli_num_rows($result);
-
-
-// Fixed solution 2 stored procedures
 $stmt = mysqli_stmt_init($conn);
-$sql = "CALL get_login('$username', '$password');";
+$sql = "CALL Get_User('$username', '$password');";
 if (mysqli_stmt_prepare($stmt, $sql)) 
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $count = mysqli_num_rows($result);
-   
+*/  
 
+/*
+// Fixed solution 3 prepared statements and stored procedures
+$stmt = mysqli_stmt_init($conn);
+$sql = "CALL get_login(?, ?);";
+if (mysqli_stmt_prepare($stmt, $sql)) {
+    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $count = mysqli_num_rows($result);
+}
+*/
 
+/*  
+//Fixed solution 4 with escaped values
+ $sql = "SELECT Username, Password FROM customers WHERE Username = '$username' AND Password = '$password'";
+ $result = mysqli_query($conn, $sql);
+ $count = mysqli_num_rows($result);
+*/  
 
-//Fixed solution 3 prepared statements and stored procedures
-//$sql = "CALL 'get_login'(?, ?);";
-//$stmt = mysqli_stmt_init($conn);
-//mysqli_stmt_prepare($stmt, $sql);
-//mysqli_stmt_bind_param($stmt, "ss", $email, $password);
-//mysqli_stmt_execute($stmt);
-//$result = mysqli_stmt_get_result($stmt);
-//$count = mysqli_num_rows($result);
-
-
-
+  
 if($count > 0)
 {
   while($row = $result->fetch_assoc()) {
@@ -92,6 +86,8 @@ if($count > 0)
 
 else {
     header("Location: modulenosignin.html");
+}
+mysqli_close($conn);
 }
 
 ?>
@@ -103,7 +99,6 @@ else {
 <form method="post" action="logout.php">
   <input type="submit" value="Logout" />
 </form>
-
 
 </body>
 </html>
